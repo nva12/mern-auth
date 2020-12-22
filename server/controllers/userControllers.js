2e003ea0b2413c976d95dca8c5f3cb11070508a1
nva12 <nvallee.a7@gmail.com>
2020-12-22 12:10:39 +0400

    user signup controller

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,5 +1,25 @@
+const User = require('../models/userModel');
+
 exports.signupUser = (req, res) => {
-  res.json({
-    data: 'You reached the signup endpoint!',
+  const { name, email, password } = req.body;
+
+  User.findOne({ email }).exec((err, user) => {
+    if (user) {
+      return res.status(400).json({ error: 'This email is already in use.' });
+    }
+  });
+
+  let newUser = new User({ name, email, password });
+
+  newUser.save((err, success) => {
+    if (err) {
+      console.log('Error signing up user: ', err);
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    res.json({
+      message: 'Successfully signed up user. Please sign in.',
+    });
   });
 };
