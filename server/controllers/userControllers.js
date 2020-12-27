c9fb9b8698087b3e607c7ff7c827d08f5ea946dd
nva12 <nvallee.a7@gmail.com>
2020-12-27 12:16:10 +0400

    send activation email on signup with sendgrid

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,6 +1,11 @@
+const jwt = require('jsonwebtoken');
+const sgMail = require('@sendgrid/mail');
 const User = require('../models/userModel');
 
-exports.signupUser = (req, res) => {
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// Sign up without email verification
+/* exports.signupUser = (req, res) => {
   const { name, email, password } = req.body;
 
   User.findOne({ email }).exec((err, user) => {
@@ -21,5 +26,49 @@ exports.signupUser = (req, res) => {
     res.json({
       message: 'Successfully signed up user. Please sign in.',
     });
+  });
+}; */
+
+// Sign up with email verification
+exports.signupUser = (req, res) => {
+  const { name, email, password } = req.body;
+
+  User.findOne({ email }).exec((err, user) => {
+    if (user) {
+      return res.status(400).json({ error: 'This email is already in use.' });
+    }
+
+    const token = jwt.sign(
+      {
+        name,
+        email,
+        password,
+      },
+      process.env.JWT_ACCOUNT_ACTIVATION,
+      { expiresIn: '30m' }
+    );
+
+    const emailData = {
+      from: process.env.EMAIL_FROM,
+      to: email,
+      subject: `Account activation link`,
+      html: `
+        <p>Please click on the following link to activate your account:</p>
+        <p><a href='${process.env.CLIENT_URL}/auth/activate/${token}'>Activate my account</a></p>
+        <hr/>
+      `,
+    };
+
+    sgMail
+      .send(emailData)
+      .then((sent) => {
+        console.log('Signup email sent.');
+        return res.json({
+          message: `Email has been sent to ${email}. Follow the instructions to activate your account.`,
+        });
+      })
+      .catch((err) => {
+        console.log('Error sending account activation email.');
+      });
   });
 };
